9a506366bb462512bc8ced7cf262b5eafafc5eb7
artmiptv <cmert.123@yandex.ru>
2020-01-19 18:13:17 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let socket = io('http://localhost:3000')
+let socket = io.connect()
 let messageContainer = $('#message-container')
 let roomContainer = $('#room-container')
 let messageForm = $('#send-container')
